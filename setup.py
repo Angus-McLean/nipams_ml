5509c3d5ce3d01522c199df28f403196067a9888
amclean <amclean@gorillagroup.com>
2020-09-10 23:49:55 -0400

    corrects setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
 setup(name='nipams_ml',
     version='0.1',
-    py_modules=['nipams_ml','nipams_ml.imports', 'nipams_ml.constants', 'nipams_ml.load', 'nipams_ml.experiments'],
+    # py_modules=['nipams_ml','nipams_ml.imports', 'nipams_ml.constants', 'nipams_ml.load', 'nipams_ml.experiments'],
+    packages=find_packages(),
 )
