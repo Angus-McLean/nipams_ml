303edd5ccff0ceacda7df630c7d71a2aeeb98793
amclean <amclean@gorillagroup.com>
2020-09-11 00:05:15 -0400

    changes to setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(name='nipams_ml',
     version='0.1',
     # py_modules=['nipams_ml','nipams_ml.imports', 'nipams_ml.constants', 'nipams_ml.load', 'nipams_ml.experiments'],
+    py_modules=['imports', 'constants', 'load', 'experiments'],
     packages=find_packages(),
 )
